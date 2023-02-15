3e5ee8ba859f32d3070c093084289ed73c7a6256
Valentino <valentinosetiawan32@gmail.com>
2023-02-15 22:41:09 +0700

    Add New Product Attributes

--- model/models.go
+++ model/models.go
@@ -5,15 +5,16 @@ import (
 )
 
 type Product struct {
-	ID         int64    `json:"id" pg:"id:bigserial pk"`
-	Name       string   `json:"name"`
-	Quantity   int      `json:"quantity"`
-	Price      int      `json:"price"`
-	Image      string   `json:"image"`
-	CategoryID int64    `json:"category_id"`
-	StoreID    int64    `json:"store_id"`
-	Store      Store    `json:"store"`
-	Category   Category `json:"category"`
+	ID          int64    `json:"id" pg:"id:bigserial pk"`
+	Name        string   `json:"name"`
+	Quantity    int      `json:"quantity"`
+	Price       int      `json:"price"`
+	Image       string   `json:"image"`
+	Description string   `json:"description"`
+	CategoryID  int64    `json:"category_id"`
+	StoreID     int64    `json:"store_id"`
+	Store       Store    `json:"store"`
+	Category    Category `json:"category"`
 }
 type Category struct {
 	ID   int64  `json:"id"`
