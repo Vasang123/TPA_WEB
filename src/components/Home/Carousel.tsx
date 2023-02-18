b49a3a30ba9b1b6641809b9cf1cf59e45ffb714f
Valentino <valentinosetiawan32@gmail.com>
2023-02-18 18:42:56 +0700

    Dynamic Carousel

--- src/components/Home/Carousel.tsx
+++ src/components/Home/Carousel.tsx
@@ -1,37 +1,50 @@
 import { useEffect, useState } from "react";
 import style from '@/styles/Home/landingpage.module.scss'
 import { transform } from "typescript";
-const Carousel = ({slides}:any) =>{
-    const [currIndex, setCurrIndex] = useState(18);
+
+interface Props {
+    slides: {
+        image: string;
+    }[];
+}
+const Carousel = ({ slides }: Props) => {
+    const [currIndex, setCurrIndex] = useState(slides.length - 1);
+    useEffect(() => {
+        // Preload images
+        slides.forEach(slide => {
+            const img = new Image();
+            img.src = slide.image;
+        });
+    }, [slides]);
     const slideStyles = {
-        backgroundImage: `url(${slides[currIndex].url})`,
+        backgroundImage: slides[currIndex]?.image ? `url(${slides[currIndex].image})` : '',
         backgroundSize: "cover",
-        backgroundPosition : "center center",
+        backgroundPosition: "center center",
         backgroundRepeat: "no-repeat",
         width: "100%",
         height: "100%",
-        zIndex : "-1",
+        zIndex: "-1",
         transition: "background-image .5s ease-out"
     }
     const prev = () => {
-        if(currIndex == 0){
-            setCurrIndex(slides.length-1)
-        }else{
-            setCurrIndex(currIndex-1)
+        if (currIndex == 0) {
+            setCurrIndex(slides.length - 1)
+        } else {
+            setCurrIndex(currIndex - 1)
         }
     }
     const next = () => {
-        if(currIndex == slides.length-1 ){
+        if (currIndex == slides.length - 1) {
             setCurrIndex(0)
-        }else{
-            setCurrIndex(currIndex+1)
+        } else {
+            setCurrIndex(currIndex + 1)
         }
     }
-    function loop(count:any){
-        if(count === slides.length-1){
+    function loop(count: any) {
+        if (count === slides.length - 1) {
             return count = 0;
-        }else{
-            return count = currIndex -1
+        } else {
+            return count = currIndex - 1
         }
         return count;
     }
@@ -45,16 +58,16 @@ const Carousel = ({slides}:any) =>{
         };
     }, [currIndex]);
     return (
-    <>
-        <button className={style.leftArrow} onClick={prev}>
-            <i className="uil uil-angle-left-b"></i>
-        </button>
-        <div style={slideStyles}>
-        </div>
-        <button className={style.rightArrow}>
-            <i className="uil uil-angle-right-b" onClick={next}></i>
-        </button>
-    </>   
+        <>
+            <button className={style.leftArrow} onClick={prev}>
+                <i className="uil uil-angle-left-b"></i>
+            </button>
+            <div style={slideStyles}>
+            </div>
+            <button className={style.rightArrow}>
+                <i className="uil uil-angle-right-b" onClick={next}></i>
+            </button>
+        </>
     )
 
 }
--- src/components/Home/LandingPage.tsx
+++ src/components/Home/LandingPage.tsx
@@ -2,29 +2,29 @@ import style from '@/styles/Home/landingpage.module.scss'
 import Carousel from './Carousel'
 import { SecondaryLandingMenu, SecondarySpanColor } from '../Other/GlobalComponent'
 import { LogoSecondary } from '../Other/LogoComponent'
+import { useEffect, useState } from 'react';
+import { Promo } from '@/types/models';
 export default function LandingPage(){
-    const slides = [
-        {url:"http://localhost:3000/carousel/1.jpg", title : '1'},
-        {url:"http://localhost:3000/carousel/2.jpg", title : '2'},
-        {url:"http://localhost:3000/carousel/3.jpg", title : '3'},
-        {url:"http://localhost:3000/carousel/4.jpg", title : '4'},
-        {url:"http://localhost:3000/carousel/5.jpg", title : '5'},
-        {url:"http://localhost:3000/carousel/6.jpg", title : '6'},
-        {url:"http://localhost:3000/carousel/7.jpg", title : '7'},
-        {url:"http://localhost:3000/carousel/8.jpg", title : '8'},
-        {url:"http://localhost:3000/carousel/9.jpg", title : '9'},
-        {url:"http://localhost:3000/carousel/10.jpg", title : '10'},
-        {url:"http://localhost:3000/carousel/11.jpg", title : '11'},
-        {url:"http://localhost:3000/carousel/12.jpg", title : '12'},
-        {url:"http://localhost:3000/carousel/13.jpg", title : '13'},
-        {url:"http://localhost:3000/carousel/14.jpg", title : '14'},
-        {url:"http://localhost:3000/carousel/15.jpg", title : '15'},
-        {url:"http://localhost:3000/carousel/16.jpg", title : '16'},
-        {url:"http://localhost:3000/carousel/17.jpg", title : '17'},
-        {url:"http://localhost:3000/carousel/18.jpg", title : '18'},
-        {url:"http://localhost:3000/carousel/19.jpg", title : '19'},
-        {url:"http://localhost:3000/carousel/20.jpg", title : '20'},
-    ]
+    const [carousel, setCarousel] = useState<Promo[]>([]);
+
+    useEffect(() => {
+      async function fetchData() {
+        const res = await fetch(`http://localhost:8000/api/carousel`);
+        const data = await res.json();
+    
+        setCarousel(data);
+      }
+    
+      fetchData();
+    }, []);
+    
+    const slides = carousel.map((item) => {
+      return {
+        image: item.image
+      };
+    });
+    
+    
     return (
         <>
             
