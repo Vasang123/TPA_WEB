aa7e8ee8ea38cd44909779462469441e9719396f
Valentino <valentinosetiawan32@gmail.com>
2023-02-19 21:55:10 +0700

    Remove Item from cart

--- src/components/Cart/CartView.tsx
+++ src/components/Cart/CartView.tsx
@@ -3,10 +3,27 @@ import { useEffect, useState } from "react"
 import style from '@/styles/Cart/cartview.module.scss'
 import { Counter } from "../Product/Counter";
 import Link from "next/link";
-import { ProductDivBg, SecondaryH1Color, SecondarySpanColor } from "../Other/GlobalComponent";
+import { Loading, ProductDivBg, SecondaryH1Color, SecondarySpanColor } from "../Other/GlobalComponent";
+function HandleDelete(event: React.MouseEvent<HTMLButtonElement>, user_id: number, product_id: number, carts: Cart[], setCarts: any) {
+    event.preventDefault();
+    fetch(`http://localhost:8000/api/cart/delete?user_id=${user_id}&product_id=${product_id}`, {
+        method: 'GET',
+        headers: { 'Content-Type': 'application/json' },
+    }).then(response => {
+        if (response.ok) {
+            alert("Item Removed")
+            const updatedCarts = carts.filter((cart) => cart.product_id !== product_id);
+            setCarts(updatedCarts);
+        } else {
+            alert(response.statusText);
+        }
+
+    }).catch(error => {
+        console.error('There was a problem with the fetch operation:', error);
+    });
+}
 export default function CartDisplay({ user_id }: any) {
     let Total = 0;
-
     const [carts, setCarts] = useState<Cart[]>([])
     useEffect(() => {
         const fetchData = async () => {
@@ -18,11 +35,18 @@ export default function CartDisplay({ user_id }: any) {
         fetchData();
     }, [user_id]);
 
-    if (carts) {
+    if (carts.length > 0) {
         carts.forEach((cart) => {
             Total += cart.product?.price * cart.quantity
         })
+    } else {
+        return (
+            <Loading>
+                <div className="loading_content">You Don&apos;t Have any Item</div>
+            </Loading>
+        )
     }
+
     return (
         <ProductDivBg className={style.cart_list}>
             {carts.length > 0 &&
@@ -53,14 +77,14 @@ export default function CartDisplay({ user_id }: any) {
                                         />
                                     </SecondarySpanColor>
                                 </div>
-                                <button className={style.delete}>
+                                <button className={style.delete} onClick={(event) => HandleDelete(event, user_id, cart.product_id, carts, setCarts)}>
                                     <i className="uil uil-trash-alt"></i>
                                     Remove
                                 </button>
                             </div>
                             <div className={style.bottom}>
                                 <SecondarySpanColor>
-                                    Subtotal : 
+                                    Subtotal :
                                     {cart.product?.price * cart.quantity}
                                 </SecondarySpanColor>
                             </div>
@@ -70,7 +94,7 @@ export default function CartDisplay({ user_id }: any) {
             }
             <SecondarySpanColor className={style.total_container}>
                 Total Price {Total}
-                <button className={style.order}> 
+                <button className={style.order}>
                     Check Out
                 </button>
             </SecondarySpanColor>
--- src/pages/cart/view.tsx
+++ src/pages/cart/view.tsx
@@ -3,17 +3,23 @@ import Footer from "@/components/Home/Footer";
 import Navbar from "@/components/Navbar/Navbar";
 import { Loading } from "@/components/Other/GlobalComponent";
 import { User } from "@/types/models";
+import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 
 export default function Cart() {
     const [userData, setUserData] = useState<User>();
     const [loading, setLoading] = useState(true);
-  
+    let i = 1;
+    const r = useRouter()
     useEffect(() => {
       const userDataString = localStorage.getItem("user");
       if (userDataString) {
         setUserData(JSON.parse(userDataString));
+      }else if (i == 1){
+        alert("You Must Login First")
+        r.push("/")
+        i = 0
       }
       setLoading(false);
     }, []);
