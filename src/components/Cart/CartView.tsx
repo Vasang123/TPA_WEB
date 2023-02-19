95f7ea893d8d4d45a47d6442176c754c77589e31
Valentino <valentinosetiawan32@gmail.com>
2023-02-19 21:14:42 +0700

    Cart Page Styling

--- src/components/Cart/CartView.tsx
+++ src/components/Cart/CartView.tsx
@@ -2,6 +2,8 @@ import { Cart } from "@/types/models";
 import { useEffect, useState } from "react"
 import style from '@/styles/Cart/cartview.module.scss'
 import { Counter } from "../Product/Counter";
+import Link from "next/link";
+import { ProductDivBg, SecondaryH1Color, SecondarySpanColor } from "../Other/GlobalComponent";
 export default function CartDisplay({ user_id }: any) {
     let Total = 0;
 
@@ -22,17 +24,20 @@ export default function CartDisplay({ user_id }: any) {
         })
     }
     return (
-        <div>
+        <ProductDivBg className={style.cart_list}>
             {carts.length > 0 &&
                 carts.map((cart) => (
                     <div key={cart.id} className={style.cart_container}>
-                        <img src={cart.product?.image} alt="" />
+                        <Link href={`/products/detail?id=${encodeURIComponent(cart.product_id)}`}>
+                            <img src={cart.product?.image} alt="" />
+                        </Link>
                         <div className={style.right_container}>
-                            <h3>{cart.product?.name}</h3>
+                            <SecondaryH1Color>{cart.product?.name}</SecondaryH1Color>
+                            <SecondarySpanColor>Price/item: {cart.product?.price}</SecondarySpanColor>
                             <div className={style.middle}>
                                 <div className={style.temp}>
-                                    <p>{cart.product?.price}</p>
-                                    <p>
+                                    <SecondarySpanColor className={style.quantity_container}>
+                                        Quantity:
                                         <Counter count={cart.quantity}
                                             setCount={
                                                 (newCount: number) => {
@@ -46,18 +51,29 @@ export default function CartDisplay({ user_id }: any) {
                                                 }}
                                             limit={cart.product?.quantity}
                                         />
-                                    </p>
+                                    </SecondarySpanColor>
                                 </div>
-                                <button>Delete</button>
+                                <button className={style.delete}>
+                                    <i className="uil uil-trash-alt"></i>
+                                    Remove
+                                </button>
                             </div>
                             <div className={style.bottom}>
-                                <p>{cart.product?.price * cart.quantity}</p>
+                                <SecondarySpanColor>
+                                    Subtotal : 
+                                    {cart.product?.price * cart.quantity}
+                                </SecondarySpanColor>
                             </div>
                         </div>
                     </div>
                 ))
             }
-            Total {Total}
-        </div>
+            <SecondarySpanColor className={style.total_container}>
+                Total Price {Total}
+                <button className={style.order}> 
+                    Check Out
+                </button>
+            </SecondarySpanColor>
+        </ProductDivBg>
     )
 }
